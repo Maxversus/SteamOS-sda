9393e423fdd302bda6ab339346758ee539b15468
Max <maxversus@yandex.ru>
2025-04-10 17:29:46 +0300

    Update install_sdcard.sh

--- install_sdcard.sh
+++ install_sdcard.sh
@@ -9,15 +9,4 @@ sudo umount /dev/sda6
 sed 's/nvme0n1/sda/g' ~/tools/repair_device.sh > ~/tools/repair_device_sda.sh
 sudo chmod +x ~/tools/repair_device_sda.sh
 
-echo
-echo vvvvvvvvvv
-echo "Please press PROCEED at the FIRST prompt to start."
-echo "Then press CANCEL on the NEXT prompt to NOT reboot the machine."
-echo ^^^^^^^^^^
-echo
-sleep 3
-
-# Run the repair script
-sudo ~/tools/repair_device_sda.sh all
-
 echo "Done!"
